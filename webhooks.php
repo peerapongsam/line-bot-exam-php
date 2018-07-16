407b8451625bd63ab56679d5d3465ddeb876a0fa
Peerapong Samarnpong <peerapong_sam@hotmail.com>
2018-07-16 17:13:57 +0700

    Change Push

--- webhooks.php
+++ webhooks.php
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['source']['userId'];
-			$text = "\n" . $event['source']['groupId'];
+			$text .= "\n" . $event['source']['groupId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
