96cbbce93a5528ef660008defcd4fb0975fa5dfc
Peerapong Samarnpong <peerapong_sam@hotmail.com>
2018-07-16 14:02:12 +0700

    Change Push

--- botpush.php
+++ botpush.php
@@ -18,14 +18,22 @@ $events = json_decode($content, true);
 
 $message = "";
 
-//Peerapong Samarnpong pushed to branch release/2.3.0 of developer/theandroid (Compare changes)
 if ($events['object_kind'] == 'push') {
+  //Peerapong Samarnpong pushed to branch release/2.3.0 of developer/theandroid (Compare changes)
   $message = $events['user_name'] . " pushed to " . str_replace('refs/heads/', '', $events['ref']) . " to " . $events['project']['path_with_namespace'];
   $commits = $events['commits'];
   if (count($commits) > 0) {
     foreach($commits as $k => $commit) {
       $message .= "\n  - " .substr($commit['id'], 0, 8) . ": " . $commit['message'] . " By " . $commit['author']['name'] ;
     }
+  }
+} else if ($events['object_kind'] == 'pipeline') {
+  //developer/theandroid: Pipeline #605 of branch release/2.3.0 by Peerapong Samarnpong (peerapongsam) passed in 12:07
+  $message = $events['project']['path_with_namespace'] . ": Pipeline #" . $events['object_attributes']['id'] . " of branch" . $events['object_attributes']['ref'];
+  $message .= " By " . $events['user']['name'] . " (" . $events['user']['username'] . ")";
+  $message .= " " . $events['object_attributes']['status'];
+  if ($events['object_attributes']['duration'] != null) {
+    $message .= " in " . $events['object_attributes']['duration'];
   }
 }
 
