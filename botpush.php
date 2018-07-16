0f52928df6e51007b0ec732277d151c34f4c6f47
Peerapong Samarnpong <peerapong_sam@hotmail.com>
2018-07-16 14:04:13 +0700

    Change Push

--- botpush.php
+++ botpush.php
@@ -29,7 +29,7 @@ if ($events['object_kind'] == 'push') {
   }
 } else if ($events['object_kind'] == 'pipeline') {
   //developer/theandroid: Pipeline #605 of branch release/2.3.0 by Peerapong Samarnpong (peerapongsam) passed in 12:07
-  $message = $events['project']['path_with_namespace'] . ": Pipeline #" . $events['object_attributes']['id'] . " of branch" . $events['object_attributes']['ref'];
+  $message = $events['project']['path_with_namespace'] . ": Pipeline #" . $events['object_attributes']['id'] . " of branch " . $events['object_attributes']['ref'];
   $message .= " By " . $events['user']['name'] . " (" . $events['user']['username'] . ")";
   $message .= " " . $events['object_attributes']['status'];
   if ($events['object_attributes']['duration'] != null) {
